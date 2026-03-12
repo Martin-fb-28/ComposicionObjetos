f02ac883ac543866ce18f87f575299353b65f298
Martin-fb-28 <martinfbao@gmail.com>
2026-03-12 13:42:00 +0100

    Acabado el ejercicio 4 de la semana 4

---
+++ src/Ejercicio4/Cuenta.java
@@ -0,0 +1,17 @@
+package Ejercicio4;
+
+public class Saldo{
+
+    private String nombre;
+    private String DNI;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public 
+}
